d1f7668f4aebc17547d10aacf635c61b7f36fc9c
pitcany <pitcany@gmail.com>
2016-04-30 10:49:01 -0700

    edits as of 10:48am

--- classification_script.m
+++ classification_script.m
@@ -1,9 +1,9 @@
-five_weights = exp(1:5)/sum(exp(1:5));
-%seven_weights = exp(1:7)/sum(exp(1:7));
-train_feature_table = features_train(train,5,five_weights);
-test_feature_table = features_test(test,5,five_weights);
-%train_feature_table = features_train(train,7,seven_weights);
-%test_feature_table = features_test(test,7,seven_weights);
+% five_weights = exp(1:5)/sum(exp(1:5));
+% train_feature_table = features_train(train,5,five_weights);
+% test_feature_table = features_test(test,5,five_weights);
+seven_weights = exp(1:7)/sum(exp(1:7));
+train_feature_table = features_train(train,7,seven_weights);
+test_feature_table = features_test(test,7,seven_weights);
 [confmat_linearsvm_ova,accuracy_linearsvm_ova]=evaluate_performance(linearSVM_ova,test_feature_table,3);
 [confmat_linearsvm,accuracy_linearsvm]=evaluate_performance(linearSVM,test_feature_table,3);
 [confmat_gaussiansvm,accuracy_gaussiansvm]=evaluate_performance(GaussianSVM,test_feature_table,3);
